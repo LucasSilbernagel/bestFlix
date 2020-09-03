492b3ebff43c4517f6f65cbf25762867ef7c0bf8
Lucas Silbernagel <lucas.silbernagel@gmail.com>
2020-09-03 14:12:34 -0400

    almost fully rendered nominated to page

--- src/Components/Nominated.js
+++ src/Components/Nominated.js
@@ -1,27 +1,55 @@
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
 function Nominated(props) {
 
-  // const [nominated, setNominated] = useState(props);
+  // return null;
 
-  return null;
+  // const handleClick = () => {
+  //   console.log(props.nominated);
+  // }
+
+  // return (
+  //   <>
+  //     <button onClick={handleClick}>console log</button>
+  //   </>
+  // );
+
+  // return (
+  //   <li>
+  //     {props.nominated.map((value, index) => {
+  //       return <p key={index}>{value}</p>
+  //     })}
+  //   </li>
+  // )
+
+  // let nominatedMovies = Object.keys(props.nominated).map(function(key) {
+  //   return <p>Key: {key}, Value: {props.nominated[key]}</p>;
+  // })
+
+  // let nominatedMovies = forEach
+
+  // return nominatedMovies;
 
   const handleClick = () => {
-    console.log(props.nominated);
+    console.log(props.Title);
   }
 
-  return (
-    <>
-      <button onClick={handleClick}>console log</button>
-    </>
-  );
-
-  let nominatedMovies = Object.keys(props.nominated).map(function(key) {
-    return <p>Key: {key}, Value: {props.nominated[key]}</p>;
+  if (!props.nominated) {
+    return null;
+  } else {
+    return  Object.keys(props.nominated).map((item,index) => {
+      return(
+        <li key={index}>
+          <p>{index.Title}</p>
+          <p>{item.Year}</p>
+          <p>{item.Plot}</p>
+          <button onClick={handleClick}>console</button>
+        </li>
+      )
   })
+  }
 
-  return nominatedMovies;
-  
+
 }
 
 export default Nominated;
