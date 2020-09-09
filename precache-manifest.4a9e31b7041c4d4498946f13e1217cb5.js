9692c8e982e1ad9f45f0342ba8b778a2c87f0aaa
Lucas Silbernagel <lucas.silbernagel@gmail.com>
2020-09-08 20:48:19 -0400

    Updates

--- precache-manifest.4a9e31b7041c4d4498946f13e1217cb5.js
+++ precache-manifest.4a9e31b7041c4d4498946f13e1217cb5.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f3a913abaa0e25829fdba58413bb683c",
+    "revision": "b1ca1bebd7ee9ae9894b6fde18ad790e",
     "url": "/shoppies/index.html"
   },
   {
-    "revision": "b53c3e4e2af6b0309f8b",
-    "url": "/shoppies/static/css/main.21749590.chunk.css"
+    "revision": "3afb95290eada6670b87",
+    "url": "/shoppies/static/css/main.65ba4dd7.chunk.css"
   },
   {
     "revision": "d9a6e3f64b134ab49f49",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/shoppies/static/js/2.2cd719d4.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "b53c3e4e2af6b0309f8b",
-    "url": "/shoppies/static/js/main.6dfd36d5.chunk.js"
+    "revision": "3afb95290eada6670b87",
+    "url": "/shoppies/static/js/main.47c1045c.chunk.js"
   },
   {
     "revision": "27778a2488007f8c9380",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/shoppies/precache-manifest.01e2eff94c84fabb14515f85eed364b8.js"
+  "/shoppies/precache-manifest.4a9e31b7041c4d4498946f13e1217cb5.js"
 );
 
 self.addEventListener('message', (event) => {
