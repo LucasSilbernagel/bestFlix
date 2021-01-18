3fcac7a9bef7a5ce014a66d5e2ff7a8a83600fec
Lucas Silbernagel <lucas.silbernagel@gmail.com>
2021-01-18 16:54:39 -0500

    added nomination counter to film button

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -55,7 +55,10 @@ function Main () {
     return (
       <main>
         <div className="wrapper">
-          <button aria-label="view your nomination list" className="filmButton" onClick={openModal}><i className="fas fa-film"></i></button>
+          <button aria-label="view your nomination list" className="filmButton" onClick={openModal}>
+            <i className="fas fa-film"></i>
+            <span className="nominatedNumber">{nominated.length}</span>
+          </button>
           <Modal
         isOpen={modalIsOpen}
         onRequestClose={closeModal}
@@ -79,12 +82,55 @@ function Main () {
         </div>
       </main>
     );
-    // If user has nominated less than 4 movies, allow them to continue to nominate more.
-  } else {
+    // If no movies have been nominated, don't show number above button
+  } else if (!nominated.length) {
     return (
       <main>
         <div className="wrapper">
           <button aria-label="view your nomination list" className="filmButton" onClick={openModal}><i className="fas fa-film"></i></button>
+          <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        className="modal"
+        overlayClassName="overlay"
+        role="dialog"
+          >
+            <button onClick={closeModal} className="sr-only">Close dialog</button>
+        {/* <button onClick={closeModal}>close</button> */}
+        <ul>
+          <Nominated nominated={nominated} setNominated={setNominated} />
+            </ul>
+            <Save nominated={nominated} setNominated={setNominated} />
+      </Modal>
+          <div className="centerWrapper">
+            <h2>Welcome to bestFlix, the best movies of all time as voted by you! Search for and nominate up to four movies, and vote for your favourites on the official list of nominees.</h2>
+          </div>
+          <Link to="/bestFlix/nominations" className="link">View nominees <span><i className="fas fa-arrow-right"></i></span></Link>
+          <div className="centerWrapper">
+          <form action="#">
+            <label htmlFor="movieTitle" className="sr-only">Movie title:</label>
+            <input onChange={handleChange} type="text" name="movieTitle" className="movieTitle" id="movieTitle" placeholder="Type a movie title" />
+            </form>
+            <div className="loadingContainer">
+              <Loading loading={loading} />
+            </div>
+          </div>
+          <ul className="results" id="results">
+            {/* Search results go here */}
+            <SearchResults movies={movies} inputValue={inputValue} nominated={nominated} setNominated={setNominated} searched={searched} />
+          </ul>
+          
+        </div>
+      </main>
+    );
+  } else {
+    return (
+      <main>
+        <div className="wrapper">
+          <button aria-label="view your nomination list" className="filmButton" onClick={openModal}>
+            <i className="fas fa-film"></i>
+            <span className="nominatedNumber">{nominated.length}</span>
+          </button>
           <Modal
         isOpen={modalIsOpen}
         onRequestClose={closeModal}
